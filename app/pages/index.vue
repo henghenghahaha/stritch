1ff3d8e6897ba748b9d4faf93c91a200c11584d8
hengworkinggit <niezhongheng@qq.com>
2024-07-14 13:03:54 +0800

    first

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,59 @@
 <script setup lang="ts">
-const { data: page } = await useAsyncData('index', () => queryContent('/').findOne())
-if (!page.value) {
-  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+const page = {
+  "title": "Stritch University",
+  "description": "",
+  "navigation": "False",
+  "hero": {
+    "title": "Cardinal Stritch University"
+  },
+  "sections": [
+    {
+      "title": "GREAT HALL",
+      "description": "The Great Hall at Stritch University stands as a testament to the institution's " +
+        "commitment to fostering a vibrant and inclusive community. This grand and elegant space, " +
+        "with its high vaulted ceilings and stunning architectural details, serves as the heart of " +
+        "the university's social and academic life. The hall is adorned with beautiful chandeliers " +
+        "that cast a warm, inviting glow over the polished wooden floors and intricately designed walls.",
+      "id": "features",
+      "img": "/greathall.png",
+      "align": "left"
+    },
+    {
+      "title": "BONAVENTURE HALL",
+      "description": "Bonaventure Hall is a cornerstone of Stritch University, embodying the spirit of academic " +
+        "excellence and community engagement. As one of the primary buildings on campus, Bonaventure " +
+        "Hall serves as a central hub for a variety of academic and administrative functions, playing " +
+        "a crucial role in the daily life of our students, faculty, and staff.",
+      "align": "right",
+      "img": "/BONAVENTUREHALL.png"
+    },
+    {
+      "title": "LIBRARY",
+      "description": "Welcome to the Stritch University Library, a vibrant hub of learning and research at the heart " +
+        "of our campus. Our library is dedicated to providing a welcoming and resource-rich environment " +
+        "that supports the academic and personal growth of our students, faculty, and staff.",
+      "align": "left",
+      "img": "/LIBRARY.png"
+    },
+    {
+      "title": "CAMPUS CENTER",
+      "description": "The Campus Center at Stritch University serves as the heart of student life and a central hub " +
+        "for the university community. This vibrant space is designed to foster a sense of belonging and " +
+        "engagement among students, faculty, and staff.",
+      "align": "right",
+      "img": "/CAMPUSCENTER.png"
+    },
+    {
+      "title": "CONFERENCE CENTER",
+      "description": "The Stritch University Conference Center is a premier venue designed to host a wide range of " +
+        "events, from academic conferences and corporate meetings to social gatherings and workshops. " +
+        "Nestled within the picturesque campus of Stritch University, the center offers state-of-the-art " +
+        "facilities, exceptional services, and a serene environment conducive to productive and memorable events.",
+      "align": "left",
+      "img": "/CONFERENCECENTER.png"
+    }
+  ]
 }
-
-useSeoMeta({
-  titleTemplate: '',
-  title: page.value.title,
-  ogTitle: page.value.title,
-  description: page.value.description,
-  ogDescription: page.value.description
-})
 </script>
 
 <template>
@@ -19,28 +62,13 @@ useSeoMeta({
       :title="page.hero.title"
       :ui="{title: 'text-3xl font-serif font-semibold tracking-tight text-gray-900 dark:text-white sm:text-7xl'}"
     >
-      <div class="absolute inset-0 landing-grid z-[-1] [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" />
+      <div
+        class="absolute inset-0 landing-grid z-[-1] [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"/>
 
-      <template #headline>
-        <UBadge
-          v-if="page.hero.headline"
-          variant="subtle"
-          size="lg"
-          class="relative rounded-full font-semibold"
-        >
-          {{ page.hero.headline.label }}
-
-          <UIcon
-            v-if="page.hero.headline.icon"
-            :name="page.hero.headline.icon"
-            class="ml-1 w-4 h-4 pointer-events-none"
-          />
-        </UBadge>
-      </template>
     </ULandingHero>
 
     <ULandingSection class="!pt-0">
-      <ImagePlaceholder />
+      <ImagePlaceholder/>
     </ULandingSection>
 
     <ULandingSection
@@ -66,15 +94,14 @@ useSeoMeta({
 <style scoped>
 .landing-grid {
   background-size: 100px 100px;
-  background-image:
-    linear-gradient(to right, rgb(var(--color-gray-200)) 1px, transparent 1px),
-    linear-gradient(to bottom, rgb(var(--color-gray-200)) 1px, transparent 1px);
+  background-image: linear-gradient(to right, rgb(var(--color-gray-200)) 1px, transparent 1px),
+  linear-gradient(to bottom, rgb(var(--color-gray-200)) 1px, transparent 1px);
 }
+
 .dark {
   .landing-grid {
-    background-image:
-      linear-gradient(to right, rgb(var(--color-gray-800)) 1px, transparent 1px),
-      linear-gradient(to bottom, rgb(var(--color-gray-800)) 1px, transparent 1px);
+    background-image: linear-gradient(to right, rgb(var(--color-gray-800)) 1px, transparent 1px),
+    linear-gradient(to bottom, rgb(var(--color-gray-800)) 1px, transparent 1px);
   }
 }
 
@@ -83,7 +110,7 @@ useSeoMeta({
   border: 1px solid #ccc;
   border-radius: 10px;
   padding: 20px;
-  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   text-align: center;
   max-width: 600px;
   position: relative;
@@ -101,7 +128,7 @@ useSeoMeta({
   border: 1px solid #ccc;
   border-radius: 10px;
   padding: 20px;
-  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   text-align: center;
   max-width: 1200px;
   position: relative;
